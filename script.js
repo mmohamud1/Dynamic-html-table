da8520d4eacefad5d2d2f512e4b01426a1b8fc29
Mohamed Mohamud <m.mohamud1@hotmail.com>
2021-03-30 14:04:50 +0100

    add colour to chart

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 var stage = anychart.graphics.create('card-graph-1');
-  
 // create charts
 chart1 = anychart.sparkline();
-chart2 = anychart.sparkline();
-
 // setting the titles
 chart1.title("EUR/USD");
-
-
 // set series type
 chart1.seriesType("area");
- 
-
 // create data for both charts
 chart1.data([1.1171,1.1241,1.1262,1.1381,1.1471]);
-
-
+// chart colours
+chart1.fill('rgba(36,214,131,0.7)')
+chart1.stroke('#24d683')
 // set charts dimensions and postition
 chart1.bounds(0, 0, 200, 50);
-
-
 // set container id for the charts
 chart1.container(stage);
 
