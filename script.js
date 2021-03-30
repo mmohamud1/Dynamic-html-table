cd0bd51d47f42e2d749f63153569a96c2ba85c75
Mohamed Mohamud <m.mohamud1@hotmail.com>
2021-03-30 14:34:21 +0100

    add fourth chart

--- script.js
+++ script.js
@@ -74,3 +74,79 @@ chart1.bounds(0, 0, 200, 50);
 chart1.container(stage);
 // initiate chart drawing
 chart1.draw();
+//////////////////////////////////////////////////////////////GRAPH 5////////////////////////////////////////////////////////////////
+var stage = anychart.graphics.create('card-graph-5');
+// create charts
+chart1 = anychart.sparkline();
+// setting the titles
+chart1.title("EUR/USD");
+// set series type
+chart1.seriesType("area");
+// create data for both charts
+chart1.data([1.1171,1.1241,1.1262,1.1381,1.1471]);
+// chart colours
+chart1.fill('rgba(36,214,131,0.4)')
+chart1.stroke('#24d683')
+// set charts dimensions and postition
+chart1.bounds(0, 0, 200, 50);
+// set container id for the charts
+chart1.container(stage);
+// initiate chart drawing
+chart1.draw();
+//////////////////////////////////////////////////////////////GRAPH 6////////////////////////////////////////////////////////////////
+var stage = anychart.graphics.create('card-graph-6');
+// create charts
+chart1 = anychart.sparkline();
+// setting the titles
+chart1.title("EUR/USD");
+// set series type
+chart1.seriesType("area");
+// create data for both charts
+chart1.data([1.3171,1.3741,1.2262,1.1781,1.1291]);
+// chart colours
+chart1.fill('rgba(255,67,64,0.4)')
+chart1.stroke('#ff4340')
+// set charts dimensions and postition
+chart1.bounds(0, 0, 200, 50);
+// set container id for the charts
+chart1.container(stage);
+// initiate chart drawing
+chart1.draw();
+//////////////////////////////////////////////////////////////GRAPH 7////////////////////////////////////////////////////////////////
+var stage = anychart.graphics.create('card-graph-7');
+// create charts
+chart1 = anychart.sparkline();
+// setting the titles
+chart1.title("EUR/USD");
+// set series type
+chart1.seriesType("area");
+// create data for both charts
+chart1.data([1.1171,1.1241,1.1162,1.1321,1.1171]);
+// chart colours
+chart1.fill('rgba(255,187,83,0.4)')
+chart1.stroke('#ffbb53')
+// set charts dimensions and postition
+chart1.bounds(0, 0, 200, 50);
+// set container id for the charts
+chart1.container(stage);
+// initiate chart drawing
+chart1.draw();
+//////////////////////////////////////////////////////////////GRAPH 8////////////////////////////////////////////////////////////////
+var stage = anychart.graphics.create('card-graph-8');
+// create charts
+chart1 = anychart.sparkline();
+// setting the titles
+chart1.title("EUR/USD");
+// set series type
+chart1.seriesType("area");
+// create data for both charts
+chart1.data([1,1,1,1,1]);
+// chart colours
+chart1.fill('rgba(36,214,131,0.4)')
+chart1.stroke('#24d683')
+// set charts dimensions and postition
+chart1.bounds(0, 0, 200, 50);
+// set container id for the charts
+chart1.container(stage);
+// initiate chart drawing
+chart1.draw();
