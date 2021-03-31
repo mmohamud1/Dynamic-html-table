4d861f2a9adc8713554e60b9abc8233066b34f3b
Mohamed Mohamud <m.mohamud1@hotmail.com>
2021-03-31 11:14:36 +0100

    generate dummy js table

--- script.js
+++ script.js
@@ -43,10 +43,27 @@ tableData = [
     ["0%", "Failed", [0, 0, 0, 0, 0]],
   ],
 ];
+var container = document.getElementById('table-container')
+var table = document.createElement('table');
 
-console.log(tableData)
+for (var i = 1; i < 4; i++){
+    var tr = document.createElement('tr');   
 
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
 
+    var text1 = document.createTextNode('Text1');
+    var text2 = document.createTextNode('Text2');
+
+    td1.appendChild(text1);
+    td2.appendChild(text2);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    table.appendChild(tr);
+}
+
+container.appendChild(table)
 
 
 
